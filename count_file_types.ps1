41e7d67dbb5e1fc7c05de5c12f5e8eb980f29cb3
Justin Lane <jclane@users.noreply.github.com>
2025-05-19 21:37:04 -0400

    init upload

---
+++ count_file_types.ps1
@@ -0,0 +1,37 @@
+param (
+    [string]$path
+)
+
+$foldersExcluded = @("\AppData", "\ProgramFiles (x86)")
+
+$extensionTypes = @{
+    "Images" = @("jpg", "png", "bmp", "gif", "tiff")
+    "Documents" = @("docx", "doc", "txt", "pdf", "rtf")
+    "Spreadsheets" = @("xls", "xlsx")
+    "Audio" = @("mp3", "wav", "flac", "aac")
+    "Videos" = @("mp4", "avi", "mov", "wmv", "mkv")
+}
+
+$counts = @{}
+
+Get-ChildItem -Path $path -Recurse -Force -File | ForEach-Object {
+    if ($_.PSIsContainer) { return } # Skip directories
+    foreach ($folder in $foldersExcluded) {
+        if ($_.DirectoryName.Contains($folder)) { return }
+    }
+    $ext = $_.Extension -replace "^\.","" # Remove the dot from extension
+    foreach ($extensionType in $extensionTypes.Keys) {
+        if ($ext -in $extensionTypes[$extensionType]) {
+            $counts[$extensionType] = ($counts[$extensionType] + 1)
+            #ii $_.FullName
+        }
+    }
+}
+
+Write-Output $counts
+Out-File -InputObject $counts -FilePath ".\file_counts.txt"
+
+foreach ($extensionType in $counts.Keys) {
+  $total = $counts[$extensionType]
+  Write-Output (100 / $total)
+}
